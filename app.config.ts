a7df1ee3e12dbc459d8b52b506d73424cccd4dd1
Benjamin Demetz <50681275+Benji377@users.noreply.github.com>
2024-01-27 21:14:20 +0100

    Changing app defaults

--- app.config.ts
+++ app.config.ts
@@ -1,41 +1,42 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Benjis Blog',
+    description: 'Welcome to my awesome blog!',
     image: {
-      src: '/social-card-preview.png',
-      alt: 'An image showcasing my project.',
+      src: '/banner.webp',
+      alt: 'The banner image of the blog',
       width: 400,
       height: 300
     },
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: 'right',
       logo: {
-        path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        path: '/logo.png',
+        pathDark: '/logo.png',
+        alt: 'Benjis Blog',
       }
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        enabled: true,
+        text: 'Benjis Blog',
+        repository: 'https://github.com/Benji377'
       },
-      navigation: true, // possible value are : true | false
-      alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      navigation: false,
+      alignment: 'center',
+      message: 'Follow me on'
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
-      linkedin: {
-        icon: 'uil:linkedin',
-        label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
+      twitter: 'https://twitter.com/Benben377',
+      github: 'https://github.com/Benji377',
+      youtube: 'https://www.youtube.com/@benji_377'
     },
     form: {
       successMessage: 'Message sent. Thank you!'
+    },
+    backToTop: {
+      text: 'Back to top',
+      icon: 'material-symbols:arrow-upward'
     }
   }
 })
