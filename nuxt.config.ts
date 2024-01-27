0444f938c974e4786846c188677e3906e59f05c6
Benjamin Demetz <50681275+Benji377@users.noreply.github.com>
2024-01-27 18:33:22 +0100

    maintenance

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/seo',
+    '@nuxthq/studio',
+    'nuxt-gtag',
+    '@nuxt/content',
+    '@nuxt/ui',
+  ],
+  site: {
+    url: 'https://benji377.netlify.app',
+    name: 'Benjis Blog',
+    description: 'Welcome to my awesome blog!',
+    defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
+  },
+  colorMode: {
+    classSuffix: ''
+  },
+  gtag: {
+    id: 'G-P82YK3ZM15'
+  }
 })
+// Maybe: https://nuxt.com/modules/nuxt-feedme
+// Maybe: https://nuxt.com/modules/prismic
+// Maybe: https://nuxt.com/modules/element-plus for components
+// Maybe: https://vite-pwa-org.netlify.app/frameworks/nuxt
